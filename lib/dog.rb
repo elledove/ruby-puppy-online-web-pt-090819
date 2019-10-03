dfb1d7e04c1dd45dfe9a4fa228a70eab716b31a6
Lauren Dove <elledoveinc@gmail.com>
2019-10-03 02:41:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog 
   
-  
   @@all=[]
   
   def self.all
     @@all
   end
+  
+  
+  def save
+    @@all << self
+  end
+  
   def initialize (name)
     @name = name 
     @@all << self
